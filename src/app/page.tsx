f3935f329eae88f0d0f51d6fea6be72546500450
Goldexcool <ogunseitangold105@gmail.com>
2024-05-07 22:56:58 +0100

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { useRouter } from 'next/navigation'
 const Page = () => {
   const router = useRouter()
   return (
-    <div>
-      <button onClick={() => router.push('/signin')}>Tap</button>
+    <div className='flex justify-center items-center w-full mt-[10%]'>
+      <button onClick={() => router.push('/signin')} className='border 2 border-color1'>Tap to go to signup page </button>
     </div>
   )
 }
